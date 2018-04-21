0a637a9a65ba4cfdd0aadc7fd85e7a0e41732aad
Liandrows <leandrohmvieira@gmail.com>
2018-04-21 11:30:13 -0300

    adição de uma minima pagina web para testar o modelo

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template("index.html")
 
+@app.route("/get")
+def get_response():
+    return True
+
+@app.route("/post")
+def post_response():
+    return True
+
+if __name__ == "__main__":
+    app.run()
